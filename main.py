3d81d4b0e80d250f7d19a87420ab6b80262da94f
Ritvik479 <gargritvik2018@gmail.com>
2025-04-13 17:19:51 +0530

    Test the app on main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import synapses
 
-model = synapses.UpliftModel()
-model.load("criteo-uplift-v2.1.csv.gz")
+# model = synapses.UpliftModel()
+# model.load("criteo-uplift-v2.1.csv.gz")
 
-model.train()
+# model.train()
+
+# Create the app instance
+app = synapses.AppBuilder()
+
+# Launch the Streamlit interface
+app.create()
+app.change()
